d1ec3a1e81f9171bc21d4770ca78d77f168a117b
Unimalia <valentinomct@gmail.com>
2026-02-23 12:22:47 +0100

    add sitemap

--- app/sitemaps.ts
+++ app/sitemaps.ts
@@ -1,16 +1,28 @@
-import type { MetadataRoute } from "next";
+import type { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://unimalia.it";
+  const baseUrl = 'https://www.unimalia.it'
 
   return [
-    { url: `${baseUrl}/`, lastModified: new Date() },
-    { url: `${baseUrl}/smarrimenti`, lastModified: new Date() },
-    { url: `${baseUrl}/ritrovati`, lastModified: new Date() },
-    { url: `${baseUrl}/smarrimento`, lastModified: new Date() },
-    { url: `${baseUrl}/miei-annunci`, lastModified: new Date() },
-    { url: `${baseUrl}/privacy`, lastModified: new Date() },
-    { url: `${baseUrl}/cookie`, lastModified: new Date() },
-    { url: `${baseUrl}/termini`, lastModified: new Date() },
-  ];
+    {
+      url: `${baseUrl}/`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/smarrimenti`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/ritrovati`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/servizi`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/professionisti`,
+      lastModified: new Date(),
+    },
+  ]
 }
