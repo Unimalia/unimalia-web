c8b6d999755fe3d409be25a79c3313108bab9066
Unimalia <valentinomct@gmail.com>
2026-02-25 11:09:58 +0100

    chore: add debug stripe env endpoint

---
+++ src/app/api/_debug/stripe/route.ts
@@ -0,0 +1,9 @@
+export async function GET() {
+  return Response.json({
+    hasStripeSecretKey: !!process.env.STRIPE_SECRET_KEY,
+    stripeSecretKeyLength: process.env.STRIPE_SECRET_KEY?.length ?? 0, // non mostra il valore
+    hasAppUrl: !!process.env.APP_URL,
+    hasStripeWebhookSecret: !!process.env.STRIPE_WEBHOOK_SECRET,
+    vercelEnv: process.env.VERCEL_ENV ?? null,
+  });
+}
