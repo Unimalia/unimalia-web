8b2e92a5d7a82bad395afe2a2f5598c855e1ad49
Unimalia <valentinomct@gmail.com>
2026-02-25 19:55:30 +0100

    Add billing status API and subscription page

--- src/app/professionisti/_components/ProShell.tsx
+++ src/app/professionisti/_components/ProShell.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { ProfessionistiBrand } from "./ProfessionistiBrand";
 import React, { useEffect, useMemo, useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
